5c600286bc1b51b51c6fded2e99ce5263f45423a
Nico Wagner <nwagner84@protonmail.com>
2020-06-11 08:20:07 +0200

    Add avy package

--- init.el
+++ init.el
@@ -72,6 +72,13 @@
   :config
   (global-auto-revert-mode t))
 
+(use-package avy
+  :ensure t
+  :bind (("M-g w" . avy-goto-word-1)
+         ("M-g f" . avy-goto-line)
+         ("C-." . avy-goto-word-or-subword-1)
+         ("C-," . avy-goto-char)))
+
 (use-package delsel
   :config
   ;; Delete selections with a keypress.
@@ -154,3 +161,15 @@
 
 (provide 'init)
 ;; init.el ends here
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages (quote (avy use-package))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
