16bed0dc4a6e10bad6a7633aa5882bf8209aec24
Nico Wagner <nwagner84@protonmail.com>
2020-06-11 13:53:02 +0200

    Enable subword-mode in prog-modes

--- init.el
+++ init.el
@@ -198,5 +198,23 @@
   :custom
   (inhibit-startup-screen t "disable startup screen"))
 
+(use-package subword
+  :hook ((prog-mode . subword-mode)))
+
 (provide 'init)
 ;; init.el ends here
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (diminish ivy projectile use-package magit company avy))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
